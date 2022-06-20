7b23436865a164b2c9f7efc0ac2525a270fa733d
gulsenkilic <glsnklc27@gmail.com>
2022-06-20 14:09:25 +0300

    framework7 project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,113 @@
+<<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <meta http-equiv="Content-Security-Policy" content="default-src * 'self' 'unsafe-inline' 'unsafe-eval' data: gap: content:">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, viewport-fit=cover">
+
+    <meta name="theme-color" content="#fff">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="msapplication-tap-highlight" content="no">
+
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <link rel="apple-touch-icon" href="assets/icons/apple-touch-icon.png">
+    <link rel="icon" href="assets/icons/favicon.png">
+
+
+    <link rel="stylesheet" href="/framework7-bundle.min.css">
+    <link rel="stylesheet" href="/app.css">
+
+    <style>
+        p.buyuk {text-transform:uppercase;}
+        h2.buyuk {text-transform:uppercase;}
+        h3.buyuk {text-transform:uppercase;}
+    </style>
+</head>
+<body>
+<div class="app">
+  <div class="panel panel-left panel-reveal panel-resizable">
+    <div class="page">
+      <div class="page-content">
+          <div class="block">     
+        
+          </div>
+          <div class="list links-list">
+          <ul>
+              <li>
+                <a href="{{ route('index') }}" class="panel-close external">ANA SAYFA</a>
+              </li>
+              <li>
+                <a href="{{ route('ogrenciaraform') }}" class=" external">OGRENCI ARA</a>
+              </li>
+              <li>
+                <a href="{{ route('ogrenciekleform') }}" class="panel-close external">OGRENCI EKLE</a>
+              </li>
+              <li>
+              <a href="#" class="panel-close external">KAPAT</a>
+              </li>
+          </ul>
+          </div>
+          </div>
+      </div>
+    <div class="panel-resize-handler"></div></div>
+      </div>
+    </div>
+<div class="page">
+<div class="navbar">
+    <div class="navbar-bg"></div>
+    <div class="navbar-inner sliding">
+      <div class="left">
+      <a href="#" data-panel=".panel-left" class="panel-open">
+        <div class="demo-iconiicon">
+        <i class="icon icon-back"></i> </div>
+          <span class="if-not-md">Side panels</span>
+        </a>
+      </div>
+      <div class="title">OGRENCILER<span class="subtitle">OGRENCI LISTESI</span></div>
+      <div class="right">
+          <a class="link"  href="{{ route('ogrenciaraform') }}">OGRENCI ARA</a> 
+      </div>
+    </div>
+  </div>
+
+  <div class="fab fab-extended fab-center-bottom" >
+    <a href="{{ route('ogrenciekleform') }}" class="external">
+      <div class="fab-text" style="color:white">YENI OGRENCI</div>
+    </a>
+  </div>
+  
+  
+
+<div class="page-content">
+ @foreach( $student as $key =>$student22)
+
+  <div class="card">
+  <div class="bg-color-gray" >
+      <div class="card-header" ><h2 style="color:white;">{{$student22->sid}}</h2></div> </div>
+      <div class="card-content card-content-padding"> 
+        <p style="color:black;" ><h3 class="buyuk">AD: {{$student22->fname}}</h3></p>
+        <p style="color:black;"><h3 class="buyuk">SOYAD: {{$student22->lname}}</h3></p>
+        <p style="color:black;"><h3 class="buyuk">BIRTHPLACE: {{$student22->birthplace}}</h3></p>
+        <p style="color:black;"><h3 >BIRTHDATE: {{$student22->birthdate}}</h3></p>  
+      </div>
+      <div class="card-footer">
+      <p><button type="button" onclick="return confirm('BU ÖĞRENCİYİ SİLMEK İSTİYOR MUSUNUZ?'),window.location='{{ URL::route('ogrencisil', ['sid'=>$student22->sid]); }}'"  class="col button button-fill color-pink">OGRENCI SIL</button></p>
+      <p><button type="button" onclick="window.location='{{ URL::route('ogrenciguncelle', ['sid'=>$student22->sid]); }}'" class="col button button-fill color-green">OGRENCI GUNCELLE</button></p> 
+      
+      </div>
+  </div>
+ 
+ @endforeach
+</div>
+</div>
+
+</div>
+<script src="/framework7-bundle.min.js"></script>
+<script src="/routes.js"></script>
+<script src="/store.js"></script>
+<script src="/app.js"></script>
+
+</body>
+</html>
---
+++ resources/views/searchform.blade.php
@@ -0,0 +1,94 @@
+<<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <meta http-equiv="Content-Security-Policy" content="default-src * 'self' 'unsafe-inline' 'unsafe-eval' data: gap: content:">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, viewport-fit=cover">
+
+    <meta name="theme-color" content="#fff">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="msapplication-tap-highlight" content="no">
+
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <link rel="apple-touch-icon" href="assets/icons/apple-touch-icon.png">
+    <link rel="icon" href="assets/icons/favicon.png">
+
+
+    <link rel="stylesheet" href="/framework7-bundle.min.css">
+    <link rel="stylesheet" href="/app.css">
+
+</head>
+<body>
+    
+<div class="page">
+<div class="navbar">
+    <div class="navbar-bg"></div>
+    <div class="navbar-inner sliding">
+      <div class="left">
+        <a href="{{ route('index') }}" class="link back external">
+          <i class="icon icon-back"></i>
+          <span class="if-not-md">Back</span>
+        </a>
+      </div>
+      <div class="title">OGRENCILER<span class="subtitle">Ogrenci Ara</span></div>
+      <div class="right">
+        <a class="link">Right</a>
+      </div>
+    </div>
+  </div>
+  <div class="page-content">
+ <div class="container">
+ <form method="GET" action="{{ route('ogrenciara') }}">{{csrf_field()}}    
+    <div class="data-table" >
+    <table class="table">
+            <tr>
+                <div class="item-inner">
+                <div class="item-title item-label">SID</div>
+                <div class="item-input-wrap">
+                <input name="sid" id="sid"  >
+                <span class="input-clear-button"></span>
+                </div>
+              </div></tr>
+                <tr>
+                <div class="item-inner">
+                <div class="item-title item-label">AD</div>
+                <div class="item-input-wrap">
+                <input name="fname" id="fname"  >
+                <span class="input-clear-button"></span>
+                </div>
+              </div></tr>
+                 <tr>
+                <div class="item-title item-label">SOYAD</div>
+                <div class="item-input-wrap">
+                <input name="lname" name="lname" >
+                </div>
+              </div></tr>
+                <tr>
+                <div class="item-title item-label">DOGUM YER</div>
+                <div class="item-input-wrap">
+                <input name="birthplace" id="birthplace" >
+                </div>
+              </div></tr>
+                <tr>
+                <div class="item-title item-label">DOGUM TARIH</div>
+                <div class="item-input-wrap">
+                <input name="birthdate" id="birthdate"  type=date  >
+                </div>
+              </div></tr>
+    <tr><button type="reset" class="col button button-fill color-pink">TEMİZLE</button></tr>
+    <tr><input name=ekle type=submit  class="col button button-fill color-gray" value="ARA"></tr>
+    </table>  </div>  
+    </form>
+    </div>
+
+    </div>
+    </div>
+</div>
+<script src="/framework7-bundle.min.js"></script>
+<script src="/routes.js"></script>
+<script src="/store.js"></script>
+<script src="/app.js"></script>
+</body>
+</html>
